a2e20ca9d8a1c5ff911d75e34936fd6b5cb85366
unknown <anohin.va@gmail.com>
2019-10-26 22:31:18 +0300

    Add multiline args

--- tests/Calc.Tests.cs
+++ tests/Calc.Tests.cs
@@ -1,5 +1,6 @@
 using calc;
 using NUnit.Framework;
+using Shouldly;
 
 namespace tests {
     public class Tests {
@@ -11,43 +12,61 @@ namespace tests {
         [Test]
         public void AddReturnsZeroForEmpty() {
             var zero = calc.Add("");
-            Assert.Zero(zero);
+            zero.ShouldBe(0);
         }
 
         [Test]
         public void AddReturnsZeroForZero() {
             var zero = calc.Add("0");
-            Assert.Zero(zero);
+            zero.ShouldBe(0);
         }
 
         [Test]
         public void AddReturns121For121() {
             var onetwoone = calc.Add("121");
-            Assert.AreEqual(onetwoone, 121);
+            onetwoone.ShouldBe(121);
         }
 
         [Test]
         public void Add2And2() {
             var result = calc.Add("2,2");
-            Assert.AreEqual(result, 4);
+            result.ShouldBe(4);
         }
 
         [Test]
         public void Add2And2And3() {
             var result = calc.Add("2,2,3");
-            Assert.AreEqual(result, 7);
+            result.ShouldBe(7);
         }
 
         [Test]
         public void Add2And2AndNegative3() {
             var result = calc.Add("2,2,-3");
-            Assert.AreEqual(result, 1);
+            result.ShouldBe(1);
         }
 
         [Test]
         public void NegativeResult() {
             var result = calc.Add("2,-3");
-            Assert.AreEqual(result, -1);
+            result.ShouldBe(-1);
+        }
+
+        [Test]
+        public void CharsIgnored() {
+            var result = calc.Add("fsdoifj,3,sodijf,2");
+            result.ShouldBe(5);
+        }
+
+        [Test]
+        public void EmptryArgsIgnored() {
+            var result = calc.Add(",,,,3,,,,,,2,,,,");
+            result.ShouldBe(5);
+        }
+
+        [Test]
+        public void AddSeveralLines() {
+            var result = calc.Add("2\n,,3\n2\n");
+            result.ShouldBe(7);
         }
     }
 }
