be5d51d70b5ff4cb595ce07bec6931b52538376e
unknown <anohin.va@gmail.com>
2019-10-26 21:30:13 +0300

    Add gitignore

---
+++ src/calc/Calc.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace calc {
+    public class Calc {
+        public int Add(string args){
+            return 0;
+        }
+    }
+}
--- src/calc/Program.cs
+++ src/calc/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 
-namespace calc
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace calc {
+    class Program {
+        static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Calc calc = new Calc();
+            Console.WriteLine(calc.Add("2,3"));
         }
     }
 }
