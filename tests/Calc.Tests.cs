87f26fe97033158f011f9f284a4bceb1acddd3ab
unknown <anohin.va@gmail.com>
2019-10-26 21:35:03 +0300

    Remove bin dirs

--- tests/Calc.Tests.cs
+++ tests/Calc.Tests.cs
@@ -1,23 +1,29 @@
-using NUnit.Framework;
 using calc;
+using NUnit.Framework;
 
 namespace tests {
     public class Tests {
-    private Calc calc = new Calc();
+        private Calc calc = new Calc();
 
         [SetUp]
-        public void Setup() {
-        }
+        public void Setup() {}
 
         [Test]
         public void AddReturnsZeroForEmpty() {
             var zero = calc.Add("");
             Assert.Zero(zero);
         }
-                [Test]
+
+        [Test]
         public void AddReturnsZeroForZero() {
             var zero = calc.Add("0");
             Assert.Zero(zero);
+        }
+
+        [Test]
+        public void AddReturns121For121() {
+            var onetwoone = calc.Add("121");
+            Assert.AreEqual(onetwoone, 121);
         }
     }
 }
